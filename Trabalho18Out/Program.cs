b0e647fba54af5d42be01c43ab09da6d44028dce
Henrique <pro.henriquerossin@gmail.com>
2025-10-18 14:40:08 -0300

    Add project files.

---
+++ Trabalho18Out/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabalho18Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
