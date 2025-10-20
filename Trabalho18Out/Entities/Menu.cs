411f1f225556d7d5a3127f9c7754c586f2395896
Henrique <pro.henriquerossin@gmail.com>
2025-10-20 11:59:13 -0300

    feat add classe estatica para fins de curiosidade

--- Trabalho18Out/Entities/Menu.cs
+++ Trabalho18Out/Entities/Menu.cs
@@ -6,23 +6,14 @@ using System.Threading.Tasks;
 
 namespace Trabalho18Out.Entities
 {
-    public class Menu
+    public static class Menu
     {
-        public static List<string> Options { get; set; } = new List<string>();
-        public static string? Title { get; set; }
-
-        public Menu(string title, List<string> options)
+        public static int Display(string title, List<string> options)
         {
-            Title = title;
-            Options = options;
-        }
-
-        public static int Display()
-        {
-            Console.WriteLine(Title);
-            for (int i = 0; i < Options.Count; i++)
+            Console.WriteLine(title);
+            for (int i = 0; i < options.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. {Options[i]}");
+                Console.WriteLine($"{i + 1}. {options[i]}");
             }
             Console.Write("Escolha uma opção válida: ");
             return int.Parse(Console.ReadLine() ?? "0");
